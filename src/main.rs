18a780d6b599fe2be354b94d72a1771071b89129
Alexandre Figueiredo <17xande@gmail.com>
2022-12-25 10:08:50 +0200

    add argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name="aos-s")]
+#[command(author="Alexandre Figueiredo")]
+#[command(version="0.0.1")]
+#[command(about="Interacts with Aruba switches API", long_about=None)]
+struct Cli {
+    #[arg(short, long)]
+    ip: String,
+
+    #[arg(short, long)]
+    port: u8,
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let url = format!("http://{}/rest/v8/ports/{}/mac-table", "192.168.16.72", "8");
+    let cli = Cli::parse();
+    get_macs(cli.ip, cli.port)
+}
+
+fn get_macs(ip: String, port: u8) -> Result<(), Box<dyn std::error::Error>> {
+    let url = format!("http://{}/rest/v8/ports/{}/mac-table", ip, port);
     let resp = reqwest::blocking::get(url)?
         .json::<serde_json::Value>();
     println!("{:#?}", resp);
