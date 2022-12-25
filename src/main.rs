eb782094902f29c3881c4319e0a4aca52d13117e
Alexandre Figueiredo <17xande@gmail.com>
2022-12-25 08:06:56 +0200

    basic request with json deserializing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let url = format!("http://{}/rest/v8/ports/{}/mac-table", "192.168.16.72", "8");
+    let resp = reqwest::blocking::get(url)?
+        .json::<serde_json::Value>();
+    println!("{:#?}", resp);
+    Ok(())
 }
+
